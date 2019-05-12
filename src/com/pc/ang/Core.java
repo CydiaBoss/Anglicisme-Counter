9c8a9829965e10d816226990666d69c8db7d0237
Andrew Wang <andre@192.168.2.17>
2019-05-12 18:55:17 -0400

    Scanner obj Added

--- src/com/pc/ang/Core.java
+++ src/com/pc/ang/Core.java
@@ -1,15 +1,27 @@
 package com.pc.ang;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Core {
 
-	public static ArrayList<File> f;
+	// File Database
+	public static File[] f;
+	// File Folder
 	public static File fold = new File("Articles");
 	
-	public static void main(String[] args) {
+	// Main
+	public static void main(String[] args) throws FileNotFoundException {
+		// Test for Folder
 		if(!(fold.exists() && fold.isDirectory()))
 			fold.mkdirs();
+		// Collect the files
+		f = fold.listFiles();
+		// Reading time
+		for(File file : f) {
+			Scanner s = new Scanner(file);
+			// TODO add reader
+		}
 	}
 }
