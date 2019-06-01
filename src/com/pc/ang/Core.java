a09c4077d12b0a9d4c1def3dcc2709f306dc53ba
Andrew Wang <andre@192.168.2.17>
2019-05-31 22:19:06 -0400

    Started creation of the file reader

--- src/com/pc/ang/Core.java
+++ src/com/pc/ang/Core.java
@@ -2,6 +2,7 @@ package com.pc.ang;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Core {
@@ -11,6 +12,7 @@ public class Core {
 	public static ArrayList<String> ss;
 	// File Folder
 	public static File fold = new File("Articles");
+	public static File result = new File("Data");
 	
 	// Main
 	public static void main(String[] args) throws FileNotFoundException {
@@ -21,13 +23,29 @@ public class Core {
 		f = fold.listFiles();
 		ss = new ArrayList<String>();
 		// Reading time
+		Scanner s = new Scanner(System.in);
 		for(File file : f) {
-			Scanner s = new Scanner(file);
+			s = new Scanner(file);
 			//Reads the Files
 			String tempS = "";
-			while(s.hasNextLine)
-			        tempS =+ s.nextLine();
+			while(s.hasNextLine())
+			        tempS += s.nextLine();
 			ss.add(tempS);
 		}
+		s.close();
+		
+		/*Counting*/
+		// Test for Folder
+		if(!(result.exists() && result.isDirectory()))
+			result.mkdirs();
+		// Filtering & Analysising
+		int count = 0;
+		for(String art : ss) {
+			// No Articles
+			art = art.replaceAll("[Ll]es |[Ll][ea] |[Ll]'|[uU]n(e)? |[dD]e(s)? |[Dd]'|[¿‡] ", "");
+			// Create New File
+			File re = new File("result\"" + f[count]);
+		}
+		
 	}
 }
