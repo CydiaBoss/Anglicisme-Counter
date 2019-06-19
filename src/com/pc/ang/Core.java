4bf743f69368466384b17981996df6bca30bc577
Andrew Wang <andre@192.168.2.17>
2019-06-19 16:03:57 -0400

    Version 2a

--- src/com/pc/ang/Core.java
+++ src/com/pc/ang/Core.java
@@ -22,20 +22,38 @@ public class Core {
 	public static ArrayList<String> ss;
 	// File Folder
 	public static File fold = new File("Articles");
-	public static File reslt = new File("Results");
+	public static File reslt = new File("Results.txt");
 	// Online Client
 	public static Checker dict;
+	// Pauser
+	public static Scanner sc = new Scanner(System.in);
 	
 	// Main
 	public static void main(String[] args) throws IOException, FailingHttpStatusCodeException, InterruptedException {
 		// Test for Folder
-		if(!(fold.exists() && fold.isDirectory()))
+		if(!(fold.exists() && fold.isDirectory())) {
 			fold.mkdirs();
+			System.out.println("Folder Created.\n"
+					+ "Please fill folder with text files.");
+			// Pause
+			sc.nextLine();
+			sc.close();
+			// Close
+			System.exit(2);
+		}
 		if(!(reslt.exists() && !reslt.isDirectory()))
 			reslt.createNewFile();
-		System.out.println("Files Found\n");
 		// Collect the files
 		f = fold.listFiles();
+		if(f.length == 0) {
+			System.out.println("No Files Found\n");
+			// Pause
+			sc.nextLine();
+			sc.close();
+			// Close
+			System.exit(1);
+		}else
+			System.out.println(f.length + " File(s) Found\n");
 		ss = new ArrayList<String>();
 		// Reading time
 		for(File file : f) {
@@ -51,25 +69,18 @@ public class Core {
 		for(String art : ss) {
 			// Word Count
 			wrdCnt += art.split(" ").length;
-//			// No Accents
-//			art = art.replaceAll("[¿‡]|[¬‚]", "a");
-//			art = art.replaceAll("[«Á]", "c");
-//			art = art.replaceAll("[…È]|[»Ë]|[ Í]|[ÀÎ]", "e");
-//			art = art.replaceAll("[ŒÓ]|[œÔ]", "i");
-//			art = art.replaceAll("[‘Ù]", "o");
-//			art = art.replaceAll("[€˚]|[Ÿ˘]", "u");
-//			// No Capitals
-//			art = art.toLowerCase();
+			// No Capitals
+			art = art.toLowerCase();
 //			// No Articles
 //			art = art.replaceAll("les |l[ea] |l'|un(e)? |de(s)? |d'|[¿‡] |au(x)? |ce(tte)?(s)? ", " ");
 //			// No Pronouns
 //			art = art.replaceAll("(-)?(je |tu |j'|noun |vous |(qu')?il(s)? |(qu')?elle(s)? |[(qu') ]on |qui )|[mts](e |')|[' ]en |[' ]y ", " ");
-//			// No Numbers
-//			art = art.replaceAll("[0-9](%)?", " ");
+			// No Numbers
+			art = art.replaceAll("[0-9](%)?", "");
 //			// No Punctuation
 //			art = art.replaceAll("[\\.,]|( )?[!\\?:;]| [´ª]", " ");
-//			// No Random Whitespace
-//			art = art.replaceAll("( )+", " ");
+			// No Random Whitespace
+			art = art.replaceAll("( )+", " ");
 			// Trim
 			art = art.trim();
 			// Counting
@@ -102,8 +113,8 @@ public class Core {
 		// Finish
 		System.out.println("\n" + toPrint + "\nDone");
 		// Pause
-		Scanner sc = new Scanner(System.in);
 		sc.nextLine();
 		sc.close();
+		System.exit(0);
 	}
 }
